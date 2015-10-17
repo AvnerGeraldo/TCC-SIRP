b309fcda970e39d21fd0803f1eb0bc2aec5ed548
Avner <avner.conceicao@hotmail.com>
2015-10-16 21:17:31 -0300

    Criando Função para botao dataHora

---
+++ application/views/administracao/admin/eventos.php
@@ -0,0 +1,52 @@
+<link rel="stylesheet" href="<?=base_url('web-files/css/admin/estiloPageAdmin.css')?>">
+<div class="container-fluid well">
+	<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12 col-lg-offset-3 col-md-offset-3 col-sm-offset-3">
+		<form name="frmEvento" method="POST" class="form-horizontal">
+			<div class="form-group">
+				<label for="txtEvento" class="col-lg-2 col-md-2 col-sm-2 col-xs-4">Evento:</label>
+				<div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+					<input type="text" name="txtEvento" id="txtEvento" placeholder="Escreve aqui o evento" class="form-control">
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="txtDescricao" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">Descrição:</label>
+				<div class="col-lg-8 col-md-8 col-sm-8 col-xs-8 col-lg-offset-2 col-md-offset-2 col-sm-offset-2 col-xs-offset-4">
+					<textarea name="txtDescricao" id="txtDescricao" placeholder="Escreva aqui a descrição do evento." class="form-control"></textarea>
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="txtDataHora" class="col-lg-2 col-md-2 col-sm-2 col-xs-4">Data e Hora:</label>
+				<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+					<input type="text" name="txtDataHora" id="txtDataHora" class="form-control">
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+					<a class="btnAddBuscaPeriodo">Buscar por período&nbsp;&nbsp;&nbsp;<i class="fa fa-caret-square-o-right"></i></a>
+				</div>
+				<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+					<input type="text" name="txtDataHoraFinal" id="txtDataHoraFinal" class="form-control">
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="txtLinkEvento" class="col-lg-2 col-md-2 col-sm-2 col-xs-4">Link do Evento:</label>
+				<div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+					<input type="text" name="txtLinkEvento" id="txtLinkEvento" placeholder="Escreve aqui o evento" class="form-control">
+				</div>
+			</div>
+			<div class="form-group">
+				<button type="button" name="btnPesquisar" class="btn btn-primary">Pesquisar</button>
+				<button type="button" name="btnCadastrar" class="btn btn-success">Cadastrar</button>
+			</div>
+		</form>
+
+		<table id="tbListaEventos" class="table table-striped">
+			<thead>
+				<tr>
+					<td>Evento</td>
+					<td>Data e Hora</td>
+					<td></td>
+				</tr>
+			</thead>
+		</table>
+	</div>
+</div>
+<script type="text/javascript" src="<?=base_url('web-files/js/funcoes/telaEventos/buscaEventos.js')?>"></script>
---
+++ web-files/js/funcoes/telaEventos/buscaEventos.js
@@ -0,0 +1,9 @@
+var url = "/sirp/";
+
+$(document).ready(function() {
+	$("#txtDataHoraFinal").parent("div").hide();
+
+	$("a.btnAddBuscaPeriodo").click(function () {
+
+	});
+});
